92333076ad17da53d06b223f906331c9f814afe3
Andre Ugolini <andreugolini@unochapeco.edu.br>
2025-11-12 21:05:51 -0300

    Commitando caso de merda e eu perca tudo

---
+++ src/Astronaut.java
@@ -0,0 +1,12 @@
+package lunarmissions;
+
+/**
+ * Representa o participante da missão que contem informações
+ * como nome, nave tripulada etc.
+ */
+
+public class Astronaut {
+
+  private String name;
+  private SpaceShip spaceShip;
+}
