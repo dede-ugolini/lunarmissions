0369c59f3c4192fa1866ce27330405a019cae6b9
Andre Ugolini <andreugolini@unochapeco.edu.br>
2025-11-19 10:24:59 -0300

    add sh para abrir a documentação

---
+++ javadoc.sh
@@ -0,0 +1,10 @@
+# Shebang
+#!/usr/bin/env bash
+
+echo "Bem vindo $USER, vamos abrir a documentação da API, digite o nome do \
+seu navegador:"
+
+read BROWSER
+
+$BROWSER ./docs/allpackages-index.html
+
