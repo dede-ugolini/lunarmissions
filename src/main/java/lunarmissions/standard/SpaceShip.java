1e0ab3621aa39344b4779941a8a2142dd439b88d
Andre Ugolini <andreugolini@unochapeco.edu.br>
2025-11-22 21:26:00 -0300

    Adicionado index e descrição as naves no enum spaceships

--- src/main/java/lunarmissions/standard/SpaceShip.java
+++ src/main/java/lunarmissions/standard/SpaceShip.java
@@ -5,25 +5,25 @@ package lunarmissions.standard;
  */
 public enum SpaceShip {
 
-  PIONEER("Pioneer 0"),
-  PIONEER_1("Pioneer 1"),
+  PIONEER(0, "Pioneer 0", ""),
+  PIONEER_1(1, "Pioneer 1", ""),
 
   /**
    * A lendaria Millenium Falcon dos Star Wars pilotada por Han Solo
    * e o fucking Chewbacca, a Millenium Falcon foi um elemento
    * chave para a vitória da Aliança Rebelde Contra o Império em
-   * muitas baalhas
+   * muitas batalhas
    */
-  MILLENIUM_FALCON("Millenium Falcon"),
+  MILLENIUM_FALCON(2, "Millenium Falcon", ""),
   /**
    * A lendária Estrela da Morte do lendário Darth Vader
    */
-  DEATH_STAR("Estrela da Morte"),
+  DEATH_STAR(3, "Estrela da Morte", "A estrela da morte de Darth Vader"),
   /**
    * Uma das naves mais memóraveis graças ao seu formato característico,
    * pilotada pelo lendário caçador de recompensas Boba Fett
    */
-  SLAVE_I("Slave I"),
+  SLAVE_I(4, "Slave I", ""),
 
   /**
    * Uma família de naves espaciais utilizado pela Alinça Rebelde,
@@ -31,22 +31,43 @@ public enum SpaceShip {
    * o modelo T-65B foi pilotado por ninguem menos que Luke Skywalker o filho de
    * Dath Vader
    */
-  X_WING("X-Wing"),
+  X_WING(5, "X-Wing", ""),
   /**
    * É característico caça do Império, com seu design ameaçador
    * assusta qualquer rebelde que vê o Império se aproxiamando com tamanho poder
    * bélico
    */
-  TIE_FIGHTER("Tie Fighter"),
-  BATWING("Batwing");
+  TIE_FIGHTER(6, "Tie Fighter", ""),
+  BATWING(7, "Batwing", "");
 
+  private final int index;
   private final String name;
+  private final String desc;
 
-  SpaceShip(String name) {
+  SpaceShip(int index, String name, String desc) {
+    this.index = index;
     this.name = name;
+    this.desc = desc;
+  }
+
+  public static String fromIndex(int index) {
+    for (SpaceShip ship : SpaceShip.values()) {
+      if (ship.getIndex() == index) {
+        return ship.getName();
+      }
+    }
+    return null;
+  }
+
+  public int getIndex() {
+    return index;
   }
 
   public String getName() {
     return name;
+  }
+
+  public String getDesc() {
+    return desc;
   }
 }
