2ccdbf46c4b2349ddc168c026c10bf1f9dce3007
Andre Ugolini <andreugolini@unochapeco.edu.br>
2025-12-06 13:50:56 -0300

    add javadoc

--- src/main/java/lunarmissions/standard/Mission.java
+++ src/main/java/lunarmissions/standard/Mission.java
@@ -8,52 +8,78 @@ import java.util.UUID;
  */
 public class Mission implements Serializable {
 
-	private final UUID uuid = UUID.randomUUID();
-	private String name;
-	private String destination;
-	private String goal;
-	private String spaceShip;
-	// private ArrayList<Astronaut> astronauts;
+  private final UUID uuid = UUID.randomUUID();
+  private String name;
+  private String destination;
+  private String goal;
+  private String spaceShip;
 
-	public UUID getID() {
-		return uuid;
-	}
+  /**
+   * @return
+   */
+  public UUID getID() {
+    return uuid;
+  }
 
-	public String getSpaceShip() {
-		return spaceShip;
-	}
+  /**
+   * @return
+   */
+  public String getSpaceShip() {
+    return spaceShip;
+  }
 
-	public void setSpaceShip(String spaceShip) {
-		this.spaceShip = spaceShip;
-	}
+  /**
+   * @param spaceShip
+   */
+  public void setSpaceShip(String spaceShip) {
+    this.spaceShip = spaceShip;
+  }
 
-	public String getGoal() {
-		return goal;
-	}
+  /**
+   * @return
+   */
+  public String getGoal() {
+    return goal;
+  }
 
-	public void setGoal(String goal) {
-		this.goal = goal;
-	}
+  /**
+   * @param goal
+   */
+  public void setGoal(String goal) {
+    this.goal = goal;
+  }
 
-	public String getDestination() {
-		return destination;
-	}
+  /**
+   * @return
+   */
+  public String getDestination() {
+    return destination;
+  }
 
-	public void setDestination(String destination) {
-		this.destination = destination;
-	}
+  /**
+   * @param destination
+   */
+  public void setDestination(String destination) {
+    this.destination = destination;
+  }
 
-	public String getName() {
-		return name;
-	}
+  /**
+   * @return
+   */
+  public String getName() {
+    return name;
+  }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+  /**
+   * @param name
+   */
+  public void setName(String name) {
+    this.name = name;
+  }
 
-	@Override
-	public String toString() {
-		return ("UUID: " + uuid + "\nNome: " + name + "\nDestino: " + destination + "\nObjetivo: " + goal
-				+ "\nNave espacial: " + spaceShip + "\n");
-	}
+  @Override
+  public String toString() {
+    return ("UUID: " + uuid + "\nNome: " + name + "\nDestino: " + destination + "\nObjetivo: " + goal
+        + "\nNave espacial: " + spaceShip + "\n");
+  }
 }
